dd495bece669df5bbe5dc4a28d83767f9deade27
Alex <alex.gyumushyan@gmail.com>
2021-04-03 12:50:35 -0400

    small changes

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use((req,res,next) => {
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
+
 //import routes
 const postsRoute = require('./routes/posts');
 app.use('/posts', postsRoute);
